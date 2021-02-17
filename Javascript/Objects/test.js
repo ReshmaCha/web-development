afa5e1cb4f5279b0f8f8a31ae5122241a10f0b50
Reshma <Rmchand@hotmail.com>
2021-02-17 16:37:25 +0100

    7 objects

---
+++ Javascript/Objects/test.js
@@ -0,0 +1,7 @@
+//todo
+
+let sportclub = {naam:"Feyenoord",type:"Voetbal",ledenaantal:"38"};
+
+// Display some data from the object:
+//todo document.getElementById("clubinfo").innerText = "De clubinformatie" + sportclub.naam.type.ledenaantal;
+
